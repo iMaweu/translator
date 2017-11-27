0d17979d62d057921caf9d345f475600513ff014
Ian_Maweu <Ian_Maweu@prqa.com>
2017-11-27 17:32:08 +0000

    Extended parser to handle numbers instead of digits.

--- parser3.cpp
+++ parser3.cpp
@@ -15,6 +15,7 @@ bool mul(const char * &s, unsigned int& result);
 bool op1(const char * &s, char &symbol);
 bool prim(const char * &s, unsigned int& result);
 bool digit(const char * &s, unsigned int& result);
+bool integer(const char * &s, unsigned int& result);
 Status parse(const char * s, int expect);
 
 bool stmt(const char * s, unsigned int& result)
@@ -62,7 +63,10 @@ bool add(const char * &s, unsigned int& result)
     }
     return true;
   }
-  return false;
+  else
+  {
+    return false;
+  }
 }
 
 bool op2(const char * &s, char &c)
@@ -97,6 +101,7 @@ bool mul(const char * &s, unsigned int& result)
             break;
           case '/': result = result / p;
             break;
+          default: return false;
           }
           continue;
         }
@@ -134,23 +139,38 @@ bool prim(const char * &s, unsigned int& result)
   if(s[0] =='(')
   {
     s++;
-    unsigned int a;
-    if(add(s, a))
-    {
-      result = a;
-    }
-    if (s == NULL || s[0] != ')')
+    if(!add(s, result) && s[0] != ')')
     {
       return false;
     }
-    s++;
-    return true;
+    else
+    {
+      s++;
+      return true;
+    }
   }
-  else if (digit(s,result))
+  else 
   {
+    return (integer(s, result));
+  }
+}
+
+bool integer(const char * &s, unsigned int& result)
+{
+  unsigned int i{ 0 };
+  if (digit(s, i))
+  {
+    result = i;
+    while (digit(s, i))
+      {
+        result = 10 * result + i;
+      }
     return true;
   }
-  return false;
+  else
+  {
+    return false;
+  }
 }
 
 bool digit(const char * &s, unsigned int& result)
@@ -191,6 +211,8 @@ int main()
   assert(parse("1+2+3;", 6) == Status::OK);
   assert(parse("1+2+3+4;", 10) == Status::OK);
 
+  assert(parse("1++;", 1) == Status::SYNTAX_ERROR);
+
   assert(parse("1;", 1) == Status::OK);
   assert(parse("1-2;", -1) == Status::OK);
   assert(parse("1-2-3;", -4) == Status::OK);
@@ -209,9 +231,24 @@ int main()
 
   assert(parse("/1/3;", 1) == Status::SYNTAX_ERROR);
   assert(parse("9//3;", 1) == Status::SYNTAX_ERROR);
-  assert(parse("9/13;", 1) == Status::SYNTAX_ERROR);
   assert(parse("9/1/;", 1) == Status::SYNTAX_ERROR);
 
+  assert(parse("(1-2)-(3-4);", 0) == Status::OK);
+  assert(parse("(1-2)-(3+4);", -8) == Status::OK);
+  assert(parse("(1-2)+(3-4);", -2) == Status::OK);
+  assert(parse("(1+2)-(3+4);", -4) == Status::OK);
+  assert(parse("(1-2)-(3-4)-(5-6);", 1) == Status::OK);
+
+  assert(parse("((4/3)/(2/1));", 0) == Status::OK);
+  assert(parse("(((8/4)/1)*5);", 10) == Status::OK);
+  assert(parse("((5/2)*(2*(7/3)));", 8) == Status::OK);
+  assert(parse("((3*4)*(1*2/(6-5)));", 24) == Status::OK);
+  assert(parse("(9/(2)/(8))+((4)-(1)/2);", 4) == Status::OK);
+
+  assert(parse("((40/3)/(2/1));", 6) == Status::OK);
+  assert(parse("(1+23)-(3+17);", 4) == Status::OK);
+  assert(parse("90/9/3;", 3) == Status::OK);
+  assert(parse("10*2*30;", 600) == Status::OK);
 
   assert(parse("(6-2)*2;", 8) == Status::OK);
   assert(parse("(6-2)*2;", 7) == Status::WRONG_RESULT);
