95ed3680b992473acf28b3780e9e53b9f4604158
Ian_Maweu <Ian_Maweu@prqa.com>
2017-11-28 16:59:45 +0000

    Extended the tree data structure to handle different types of tree nodes. Fixed the print function to work with the different types of tree nodes.

--- ast.cpp
+++ ast.cpp
@@ -1,36 +1,58 @@
 #include<iostream>
-
-struct Tree
-{
-  Tree * left;
-  Tree * right;
-};
+#include "ast.h"
 
 void print(const Tree * tree)
 {
   if (tree == nullptr)
   {
     std::cout << "NULL";
+    return;
   }
-  else
+
+  switch (tree->type)
   {
-    std::cout << "Tree(";
-    print(tree->left);
-    std::cout << ",";
-    print(tree->right);
+  case TreeType::NUM:
+  {
+    std::cout << "NUM(";
+    std::cout << tree->payload.value;
     std::cout << ")";
+    break;
   }
-
+  case TreeType::PRIM:
+  {
+    std::cout << "PRIM( ";
+    print(tree->payload.branch);
+    std::cout << " )";
+    break;
+  }
+  case TreeType::MUL:
+  {
+    std::cout << "MUL( ";
+    print(tree->payload.branches.left);
+    std::cout << ", ";
+    print(tree->payload.branches.right);
+    std::cout << " )";
+    break;
+  }
+  case TreeType::ADD:
+  {
+    std::cout << "ADD( ";
+    print(tree->payload.branches.left);
+    std::cout << ", ";
+    print(tree->payload.branches.right);
+    std::cout << " )";
+    break;
+  }
+  }
 }
 
-Tree * generateTree()
+/*Tree * generateTree(TreeType treeType, TreePayload treePayload)
 {
-  Tree * root = new Tree{ nullptr, nullptr };
-  root->left = new Tree{nullptr, nullptr};
-  return root;
-}
+    Tree * tree = new Tree{ treeType, treePayload };
+  return tree;
+}*/
 
-void clearTree(Tree * tree)
+/*void clearTree(Tree * tree)
 {
   if (tree == nullptr)
   {
@@ -39,14 +61,44 @@ void clearTree(Tree * tree)
   clearTree(tree->left);
   clearTree(tree->right);
   delete tree;
+}*/
+
+void print_test()
+{
+  Tree * t1 = new Tree{ TreeType::ADD,{nullptr,nullptr} };
+  print(t1);
+  std::cout << "\n";
+
+  Tree * t2 = new Tree{ TreeType::NUM, 6 };
+  print(t2);
+  std::cout << "\n";
+
+  Tree * t3 = new Tree{ TreeType::PRIM, {t2} };
+  print(t3);
+  std::cout << "\n";
+
+  Tree * t4 = new Tree{ TreeType::PRIM, {nullptr} };
+  print(t4);
+  std::cout << "\n";
+
+  Tree * t5 = new Tree{ TreeType::PRIM, t1 };
+  print(t5);
+  std::cout << "\n";
+
+  Tree * t6 = new Tree{ TreeType::MUL,{nullptr,nullptr} };
+  print(t6);
+  std::cout << "\n";
+
+  Tree * t7 = new Tree{ TreeType::MUL,{t4,t4} };
+  print(t7);
+  std::cout << "\n";
 }
 
 int main()
 {
-  Tree * t = generateTree();
-  print(t);
-  clearTree(t);
-  //print(t);
+  print_test();
+  // Tree * t = generateTree();
+  //clearTree(t);
   char wait; std::cin >> wait;
   return 0;
 }
---
+++ ast.h
@@ -0,0 +1,32 @@
+#ifndef AST_H
+#define AST_H
+struct Tree;
+
+enum class TreeType { ADD, MUL, PRIM, NUM };
+
+union TreePayload
+{
+  TreePayload::TreePayload() {}
+  TreePayload::TreePayload(Tree * _branch) : branch(_branch) {}
+  TreePayload::TreePayload(int _value) : value(_value) {}
+  TreePayload::TreePayload(Tree * _left, Tree * _right) 
+  {
+    branches.left = _left;
+    branches.right = _right;
+  }
+
+  int value;
+  Tree * branch;
+  struct
+  {
+    Tree * left;
+    Tree * right;
+  } branches;
+};
+
+struct Tree
+{
+  TreeType type;
+  TreePayload payload;
+};
+#endif
