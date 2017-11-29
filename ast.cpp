3195e98438a3efc687bfff1830d18715cdd6a012
Ian_Maweu <Ian_Maweu@prqa.com>
2017-11-29 18:18:24 +0000

    Modified a payload type, allowing it to store an operator.

--- ast.cpp
+++ ast.cpp
@@ -99,7 +99,7 @@ Tree * generate_num1(Tree * branch)
 
 Tree *generate_num2(Tree *branch1, Tree *branch2)
 {
-  return new Tree{ TreeType::NUM2,{branch1,branch2} };
+  return new Tree{ TreeType::NUM2,{branch1,branch2,'\0'} };
 }
 
 Tree * generate_prim(Tree * branch)
@@ -112,20 +112,22 @@ Tree * generate_add1(Tree * branch)
   return new Tree{ TreeType::ADD1, {branch}};
 }
 
-Tree * generate_add2(Tree * branch1, Tree* branch2)
+Tree * generate_add2(Tree * branch1, Tree* branch2, char op)
 {
-  return new Tree{ TreeType::ADD2, {branch1,branch2}};
+  return new Tree{ TreeType::ADD2, {branch1,branch2,op}};
 }
+
 
 Tree * generate_mul1(Tree * branch)
 {
   return new Tree{ TreeType::MUL1, {branch}};
 }
 
-Tree * generate_mul2(Tree * branch1, Tree* branch2)
+Tree * generate_mul2(Tree * branch1, Tree* branch2, char op)
 {
-  return new Tree{ TreeType::MUL2, {branch1,branch2}};
+  return new Tree{ TreeType::MUL2, {branch1,branch2,op}};
 }
+
 
 Tree * generate_digit(int d)
 {
@@ -199,8 +201,8 @@ void clear(Tree * tree)
 
 void test()
 {
-  Tree * t1 = generate_add2(generate_mul2(generate_prim(generate_num1(generate_digit(0))),generate_prim(generate_num1(generate_digit(4))) ),
-                            generate_mul2(generate_prim(generate_num1(generate_digit(1))),generate_prim(generate_num1(generate_digit(3))) ));
+  Tree * t1 = generate_add2(generate_mul2(generate_prim(generate_num1(generate_digit(0))),generate_prim(generate_num1(generate_digit(4))),'/' ),
+                            generate_mul2(generate_prim(generate_num1(generate_digit(1))),generate_prim(generate_num1(generate_digit(3))),'*' ),'-');
   printTree(t1);
   clear(t1);
 
@@ -220,21 +222,76 @@ void test()
   printTree(t5);
   clear(t5);
 
-  Tree * t6 = generate_mul2(generate_prim(generate_num1(generate_digit(0))),generate_prim(generate_num1(generate_digit(4))) );
+  Tree * t6 = generate_mul2(generate_prim(generate_num1(generate_digit(0))),generate_prim(generate_num1(generate_digit(4))),'/');
   printTree(t6);
   clear(t6);
 
-  Tree * t7 = generate_mul2(generate_prim(nullptr), generate_prim(nullptr));
+  Tree * t7 = generate_mul2(generate_prim(nullptr), generate_prim(nullptr),'*');
   printTree(t7);
   clear(t7);
 
-  Tree * t8 = generate_mul2(generate_prim(generate_num1(generate_digit(8))), nullptr);
+  Tree * t8 = generate_mul2(generate_prim(generate_num1(generate_digit(8))), nullptr,'*');
   printTree(t8);
   clear(t8);
 
 }
 
-int main1()
+/*int calculateTree(Tree * tree)
+{
+  if (tree == nullptr)
+  {
+    return 0;
+  }
+  switch (tree->type)
+  {
+  case TreeType::STMT:
+  {
+    clear(tree->payload.branch);
+    break;
+  }
+  case TreeType::ADD1:
+  {
+    break;
+  }
+  case TreeType::ADD2:
+  {
+    clear(tree->payload.branches.left);
+    clear(tree->payload.branches.right);
+    break;
+  }
+  case TreeType::MUL1:
+  {
+    clear(tree->payload.branch);
+    break;
+  }
+  case TreeType::MUL2:
+  {
+    clear(tree->payload.branches.left);
+    clear(tree->payload.branches.right);
+    break;
+  }
+  case TreeType::PRIM:
+  {
+    clear(tree->payload.branch);
+    break;
+  }
+  case TreeType::NUM1:
+  {
+    return calculateTree(tree->payload.branch);
+  }
+  case TreeType::NUM2:
+  {
+    return (calculateTree(tree->payload.branches.left) * 10) + calculateTree(tree->payload.branches.right);
+  }
+  
+  case TreeType::DIGIT:
+  {
+    return tree->payload.value;
+  }
+  }
+}*/
+
+int main()
 {
   test();
   char wait; std::cin >> wait;
--- ast.h
+++ ast.h
@@ -2,16 +2,17 @@
 #define AST_H
 struct Tree;
 
-enum class TreeType { STMT,ADD1,ADD2, MUL1, MUL2, PRIM, NUM1, NUM2, DIGIT};
+enum class TreeType { STMT,ADD1,ADD2, MUL1, MUL2, PRIM, NUM1, NUM2, DIGIT, SUB2, DIV2};
 
 union TreePayload
 {
   TreePayload::TreePayload(Tree * _branch) : branch(_branch) {}
   TreePayload::TreePayload(int _value) : value(_value) {}
-  TreePayload::TreePayload(Tree * _left, Tree * _right) 
+  TreePayload::TreePayload(Tree * _left, Tree * _right, char _operation) 
   {
     branches.left = _left;
     branches.right = _right;
+    branches.operation = _operation;
   }
 
   int value;
@@ -20,6 +21,7 @@ union TreePayload
   {
     Tree * left;
     Tree * right;
+    char operation;
   } branches;
 };
 
@@ -29,18 +31,22 @@ struct Tree
   TreePayload payload;
 };
 
-void print(const Tree * tree);
 Tree *generate_num1(Tree *);
 Tree *generate_num2(Tree *,Tree *);
 Tree *generate_prim(Tree *);
 Tree *generate_add1(Tree *);
 Tree *generate_mul1(Tree *);
-Tree *generate_add2(Tree *,Tree*);
-Tree *generate_mul2(Tree *,Tree*);
+Tree *generate_add2(Tree *,Tree*,char);
+Tree *generate_mul2(Tree *,Tree*, char);
 Tree *generate_stmt(Tree*);
 Tree *generate_digit(int);
+
 void clear(Tree * tree);
+
 void printTree(const Tree * tree);
+void print(const Tree * tree);
+
 void test();
 
+int calculateTree(Tree *);
 #endif
