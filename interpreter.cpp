10ecec077cbc7204f21d06671c7c24a4f804bfe2
Ian_Maweu <Ian_Maweu@prqa.com>
2017-12-03 14:12:38 +0000

    Modified print and tree evaluation functions to comply with new AST structure.

--- interpreter.cpp
+++ interpreter.cpp
@@ -2,105 +2,36 @@
 
 #include "ast.h"
 
-int calculateStmt(TreeStmt * tree)
+int calculateTree(Tree * tree)
 {
   if (tree == nullptr)
   {
     // TODO: track errors
     assert(false);
   }
-  return calculateAdd(tree->add);
-}
-
-int calculateAdd(TreeAdd * tree)
-{
-  if (tree == nullptr)
+  else if (tree->type == Tree::TreeType::NUMBER)
   {
-    // TODO: track errors
-    assert(false);
+    return tree->value.number;
   }
-  else if(tree->type == TreeAdd::TreeAddType::ADD1)
+  else if (tree->type == Tree::TreeType::OPER)
   {
-    return calculateMul(tree->payload.add1);
-  }
-  else if (tree->type == TreeAdd::TreeAddType::ADD2)
-  {
-    switch (tree->payload.add2.operation)
+    switch (tree->value.oper.operType)
     {
     case '+':
-      return calculateAdd(tree->payload.add2.add) + calculateMul(tree->payload.add2.mul);
+      return calculateTree(tree->value.oper.left) +
+        calculateTree(tree->value.oper.right);
+
     case '-':
-      return calculateAdd(tree->payload.add2.add) - calculateMul(tree->payload.add2.mul);
-    }
-    assert(false);
-  }
-}
+      return calculateTree(tree->value.oper.left) - 
+        calculateTree(tree->value.oper.right);
 
-int calculateMul(TreeMul * tree)
-{
-  if (tree == nullptr)
-  {
-
-  }
-  else if (tree->type == TreeMul::TreeMulType::MUL1)
-  {
-    return calculatePrim(tree->payload.mul1);
-  }
-  else if (tree->type == TreeMul::TreeMulType::MUL2)
-  {
-    switch (tree->payload.mul2.operation)
-    {
     case '*':
-      return calculateMul(tree->payload.mul2.mul) * calculatePrim(tree->payload.mul2.prim);
+      return calculateTree(tree->value.oper.left) * 
+        calculateTree(tree->value.oper.right);
+
     case '/':
-      return calculateMul(tree->payload.mul2.mul) / calculatePrim(tree->payload.mul2.prim);
+      return calculateTree(tree->value.oper.left) / 
+        calculateTree(tree->value.oper.right);
     }
-    assert(false);
   }
-}
-
-int calculatePrim(TreePrim * tree)
-{
-  if (tree == nullptr)
-  {
-    // TODO: track errors
-    assert(false);
-  }
-  else if (tree->type == TreePrim::TreePrimType::NUM)
-  {
-    return calculateNum(tree->payload.num);
-  }
-  else if (tree->type == TreePrim::TreePrimType::ADD)
-  {
-    return calculateAdd(tree->payload.add);
-  }
-  assert(false);
-}
-
-int calculateNum(TreeNum * tree)
-{
-  if (tree == nullptr)
-  {
-    // TODO: track errors
-    assert(false);
-  }
-  else if (tree->type == TreeNum::TreeNumType::NUM1)
-  {
-    return calculateDigit(tree->payload.num1);
-  }
-  else if (tree->type == TreeNum::TreeNumType::NUM2)
-  {
-    return (calculateNum(tree->payload.num2.num)*10) + calculateDigit(tree->payload.num2.digit);
-  }
-  assert(false);
-}
-
-int calculateDigit(TreeDigit * tree)
-{
-  if (tree == nullptr)
-  {
-    // TODO: track errors
-    assert(false);
-  }
-  return tree->value;
 }
--- print.cpp
+++ print.cpp
@@ -2,119 +2,29 @@
 
 #include "ast.h"
 
-void printStmt(const TreeStmt * tree)
+void print(Tree * tree)
 {
   if (tree == nullptr)
   {
-    printf("NULL");
     return;
   }
-  printf("Stmt( ");
-  printAdd(tree->add);
-  printf(" )");
-}
-
-void printAdd(const TreeAdd * tree)
-{
-  if (tree == nullptr)
+  if (tree->type == Tree::TreeType::NUMBER)
   {
-    printf("NULL");
+    printf("( %d )", tree->value.number);
     return;
   }
-  switch (tree->type)
+  else if (tree->type == Tree::TreeType::OPER)
   {
-  case TreeAdd::TreeAddType::ADD1:
-    printf("Add1( ");
-    printMul(tree->payload.add1);
-    printf(")");
-    return;
-  case TreeAdd::TreeAddType::ADD2:
-    printf("Add2( ");
-    printAdd(tree->payload.add2.add);
-    printf(", ");
-    printMul(tree->payload.add2.mul);
+    printf("( ");
+    print(tree->value.oper.left);
+    printf(" %c ", tree->value.oper.operType);
+    print(tree->value.oper.right);
     printf(" )");
-    return;
   }
 }
 
-void printMul(const TreeMul * tree)
+void printTree(Tree * tree)
 {
-  if (tree == nullptr)
-  {
-    printf("NULL");
-    return;
-  }
-  switch (tree->type)
-  {
-  case TreeMul::TreeMulType::MUL1:
-    printf("Mul1( ");
-    printPrim(tree->payload.mul1);
-    printf(" )");
-    return;
-  case TreeMul::TreeMulType::MUL2:
-    printf("Mul2( ");
-    printMul(tree->payload.mul2.mul);
-    printf(", ");
-    printPrim(tree->payload.mul2.prim);
-    printf(" )");
-    return;
-  }
-}
-
-void printPrim(const TreePrim * tree)
-{
-  if (tree == nullptr)
-  {
-    printf("NULL");
-    return;
-  }
-  switch (tree->type)
-  {
-  case TreePrim::TreePrimType::NUM:
-    printf("Prim( ");
-    printNum(tree->payload.num);
-    printf(" )");
-    return;
-  case TreePrim::TreePrimType::ADD:
-    printf("Prim( ");
-    printAdd(tree->payload.add);
-    printf(" )");
-    return;
-  }
-}
-
-void printNum(const TreeNum * tree)
-{
-  if (tree == nullptr)
-  {
-    printf("NULL");
-    return;
-  }
-  switch (tree->type)
-  {
-  case TreeNum::TreeNumType::NUM1:
-    printf("Num1( ");
-    printDigit(tree->payload.num1);
-    printf(" )");
-    return;
-  case TreeNum::TreeNumType::NUM2:
-    printf("Num2( ");
-    printNum(tree->payload.num2.num);
-    printf(", ");
-    printDigit(tree->payload.num2.digit);
-    printf(" )");
-    return;
-  }
-}
-
-void printDigit(const TreeDigit * tree)
-{
-  if (tree == nullptr)
-  {
-    printf("NULL");
-    return;
-  }
-  printf("Digit ( %d )", tree->value);
-  return;
+  print(tree);
+  printf("\n");
 }
